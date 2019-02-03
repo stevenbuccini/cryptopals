7c8551c2b6793167e22f87e71b84de213631dfd5
Steven Buccini <steven@stevenbuccini.com>
2019-02-03 22:01:04 +0900

    Challenge 5

--- set1.py
+++ set1.py
@@ -69,3 +69,13 @@ def find_encrypted_string():
                 input_with_highest_score = line
 
     return input_with_highest_score
+
+#####################
+###  Challenge 5  ###
+#####################
+def repeating_key_xor(input_str, key):
+    return hexlify(bytes([ord(key[i % len(key)]) ^ ord(char) for i, char in enumerate(input_str)]))
+
+# Unclear whether or not there is supposed to be a newline in the output -- the challenge page says yes, but this works as is.
+assert(repeating_key_xor("""Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal""", 'ICE') == b'0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f')
